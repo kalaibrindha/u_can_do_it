f39f68f0586d3ec030c30f1e3c3287079a8a16c8
paapa <paapa@localhost>
2023-08-01 21:54:09 +0530

    using recursion added

--- newproject/src/newproject/RecursivelyAddDigits.java
+++ newproject/src/newproject/RecursivelyAddDigits.java
@@ -4,7 +4,10 @@ public class RecursivelyAddDigits {
 
 	public static void main(String[] args) {
 
-		int num = 9875;
+		int num = 89;
+		int ans2 = using_recursion(num);
+		System.out.println(ans2);
+
 		int sum = 0;
 		while (num > 0) {
 			int rem = num % 10;
@@ -18,6 +21,24 @@ public class RecursivelyAddDigits {
 			}
 		}
 		System.out.println("ans :" + sum);
+
+	}
+
+	private static int using_recursion(int num) {
+
+		int sum = 0;
+		if (num >= 0 && num <= 9) {
+			return num;
+		}
+		while (num > 0) {
+			int rem = num % 10;
+			sum = sum + rem;
+			num = num / 10;
+		}
+		// System.out.println(num+" "+sum);
+		int n = using_recursion(sum);
+		// System.out.println("n :"+n);
+		return n;
 
 	}
 
