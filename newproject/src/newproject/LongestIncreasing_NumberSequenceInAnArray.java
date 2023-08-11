9353e53f0bfbac07127bc744cb720054b0c0f42b
paapa <paapa@macbook-air>
2023-08-11 14:21:17 +0530

    11-8-2023 new method

--- newproject/src/newproject/LongestIncreasing_NumberSequenceInAnArray.java
+++ newproject/src/newproject/LongestIncreasing_NumberSequenceInAnArray.java
@@ -3,22 +3,34 @@ package newproject;
 public class LongestIncreasing_NumberSequenceInAnArray {
 
 	public static void main(String[] args) {
-		int[] num = { 10, 9, 2, 5, 3, 7, 101, 18 };
-		//int[] num={0,1,0,3,2,3};
-		//int[] num=	{7,7,7,7,7,7,7};
+		//int[] num = { 10, 9, 2, 5, 3, 7, 101, 18 };
+		// int[] num={10,1,0,3,2,3,87,0,98,555};
+		 int[] num= {7,7,7,7,7,7,7};
 		int length = find_length(num);
-		System.out.println("ans"+length);
+		System.out.println("ans" + length);
+		method2(num);
 
+	}
+
+	private static void method2(int[] num) {
+		int len = 1;
+		for (int i = num.length - 2; i >= 0; i--) {
+			if (num[i] < num[i + 1]) {
+				len++;
+			}
+
+		}
+		System.out.println("method 2 ans : "+len);
 	}
 
 	private static int find_length(int[] num) {
 
 		int maxvalue = 0;
 		int length = 0;
-		for (int i = 0; i < num.length-1; i++) {
+		for (int i = 0; i < num.length - 1; i++) {
 			int count = 1;
 			maxvalue = num[i];
-			for (int j = i+1; j < num.length; j++) {
+			for (int j = i + 1; j < num.length; j++) {
 				if (maxvalue < num[j]) {
 					maxvalue = num[j];
 					count++;
@@ -30,7 +42,7 @@ public class LongestIncreasing_NumberSequenceInAnArray {
 			}
 
 		}
-	//	System.out.println(length);
+		// System.out.println(length);
 		return length;
 	}
 
